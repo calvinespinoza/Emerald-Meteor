e4b888c1986edf9c6d75c1d99d2a820ee0d8495a
calvinespinoza <calvin_espinoza@hotmail.com>
2018-07-01 23:49:08 -0600

    ux functionality

---
+++ client/main.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import './main.html';
+
+Template.body.helpers({
+  notes:[
+    {text: 'My Note 1'},
+    {text: 'My Note 2'},
+    {text: 'My Note 3'}
+  ]
+})
